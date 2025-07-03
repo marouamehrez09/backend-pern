700fb81b68fe6a6c8b0a5daa5273147f794711a8
marouamehrez09 <maroua.mehrez09@gmail.com>
2025-07-03 17:02:39 +0200

    opened register to public

--- routes/employeRoute.js
+++ routes/employeRoute.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
-    getAllEmployes,
-    registerEmploye,
-    getEmployeById,
-    updateEmploye,
-    deleteEmploye,
-    getProfile,
-} = require('../controllers/employeController');
-const protect = require('../middlewares/authMiddleware');
-const isAdmin = require('../middlewares/isAdmin');
-
+  getAllEmployes,
+  registerEmploye,
+  getEmployeById,
+  updateEmploye,
+  deleteEmploye,
+  getProfile,
+} = require("../controllers/employeController");
+const protect = require("../middlewares/authMiddleware");
+const isAdmin = require("../middlewares/isAdmin");
 
 router.get("/profile", protect, getProfile);
-router.get('/', protect, getAllEmployes);
-router.post('/register', protect, isAdmin, registerEmploye);             
-router.get('/:id', protect, isAdmin, getEmployeById);           
-router.put('/:id', protect, isAdmin, updateEmploye);             
-router.delete('/:id', protect, isAdmin, deleteEmploye);          
+router.get("/", protect, getAllEmployes);
+router.post("/register", registerEmploye);
+router.get("/:id", protect, isAdmin, getEmployeById);
+router.put("/:id", protect, isAdmin, updateEmploye);
+router.delete("/:id", protect, isAdmin, deleteEmploye);
 
 module.exports = router;
