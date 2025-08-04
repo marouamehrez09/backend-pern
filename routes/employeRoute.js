6c1b5fd46530bc3a58e3b114e2d61ba3b1700930
marouamehrez09 <maroua.mehrez09@gmail.com>
2025-08-04 15:33:03 +0200

    creer un admin

--- routes/employeRoute.js
+++ routes/employeRoute.js
@@ -13,7 +13,7 @@ const isAdmin = require("../middlewares/isAdmin");
 
 router.get("/profile", protect, getProfile);
 router.get("/", protect, getAllEmployes);
-router.post("/register", protect, isAdmin, registerEmploye);
+router.post("/register", registerEmploye);
 router.get("/:id", getEmployeById);
 router.put("/:id", protect, updateEmploye);
 router.delete("/:id", protect, isAdmin, deleteEmploye);
